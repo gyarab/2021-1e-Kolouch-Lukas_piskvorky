dee97a8cf96cc54332a57dabebdddd56b1369553
LukasKolouch <94891121+LukasKolouch@users.noreply.github.com>
2022-05-03 01:11:25 +0200

    Update HelloApplication.java

--- HelloApplication.java
+++ HelloApplication.java
@@ -11,8 +11,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 600
+
+        );
+        stage.setTitle("Piškvorky");
         stage.setScene(scene);
         stage.show();
     }
